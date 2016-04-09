5a42d58e0a91ef436d458f67884afbac366c1ed3
James <theman123>
2016-04-09 14:36:14 -0700

    test

--- src/common/Constants.java
+++ src/common/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
 	public static final int ONE = 1;
 	public static final int TWO = 2;
 	
-	public static final int PORT_ZERO = 9537;
+	public static final int PORT_ZERO = 0000;
 	public static final int PORT_ONE = 9001;
 	public static final int PORT_TWO = 9002;
 	public static final int PORT_THREE = 9003;

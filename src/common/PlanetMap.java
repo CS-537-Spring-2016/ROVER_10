a1f9313da4deda02cb177755b89e288542489034
Priyank Patel <priyank1993pp@gmail.com>
2016-04-16 15:39:38 -0700

    gbgfbf

--- src/common/PlanetMap.java
+++ src/common/PlanetMap.java
@@ -36,6 +36,7 @@ public class PlanetMap {
 		this.planetMap[8][24] = new MapTile("R");
 		this.planetMap[8][25] = new MapTile("R");
 		
+		this.planetMap[10][10] = new MapTile("G");
 		this.planetMap[24][10] = new MapTile("S");
 		this.planetMap[24][11] = new MapTile("S");
 		this.planetMap[24][12] = new MapTile("S");

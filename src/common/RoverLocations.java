7606c94d79d6fc56a488ba5f1dd82649e0594c01
qucarl <qucarl@gmail.com>
2016-04-16 14:48:47 -0700

    init our Rover 10 location
    init our Rover 10 location

--- src/common/RoverLocations.java
+++ src/common/RoverLocations.java
@@ -21,7 +21,7 @@ public class RoverLocations {
 //		roverHash.put(RoverName.ROVER_07, new Coord(7,1));
 //		roverHash.put(RoverName.ROVER_08, new Coord(8,1));
 //		roverHash.put(RoverName.ROVER_09, new Coord(9,1));
-//		roverHash.put(RoverName.ROVER_10, new Coord(10,1));
+		roverHash.put(RoverName.ROVER_10, new Coord(10,1));
 //		roverHash.put(RoverName.ROVER_11, new Coord(11,1));
 //		roverHash.put(RoverName.ROVER_12, new Coord(12,1));
 //		roverHash.put(RoverName.ROVER_13, new Coord(13,1));

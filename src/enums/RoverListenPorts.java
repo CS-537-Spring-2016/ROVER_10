6b5c3945801bc4f4996962309fc70b7553dcb55d
James <theman123>
2016-05-13 16:33:29 -0700

    added communication code from rover_11

--- src/enums/RoverListenPorts.java
+++ src/enums/RoverListenPorts.java
@@ -1,37 +1,53 @@
 package enums;
 
 public enum RoverListenPorts {
-	ROVER_00 (4000),
-	ROVER_99 (4001),
-	ROVER_10 (4002),
-	DEFAULT (10000);
-	
+	ROVER_10(5000), ROVER_11(5001), ROVER_12(5002), ROVER_13(5003), ROVER_14(5004), ROVER_15(5005), ROVER_16(
+			5006), ROVER_17(5007), ROVER_18(5008), DEFAULT(10000);
+
 	private int port;
-	
+
 	public int getPort() {
 		return this.port;
 	}
-	
+
 	private RoverListenPorts(int port) {
 		this.port = port;
 	}
-	
-	public static RoverListenPorts getEnum(String input){
-    	RoverListenPorts output;
-    	
-    	switch(input){
-    	case "ROVER_10":
-    		output = RoverListenPorts.ROVER_10;
-    		break;
-    	case "ROVER_00":
-    		output = RoverListenPorts.ROVER_00;
-    		break;
-    	case "ROVER_99":
-    		output = RoverListenPorts.ROVER_99;
-    		break;   	
-    	default:
-    		output = RoverListenPorts.DEFAULT;
-    	}	
-    	return output;
-    }
+
+	public static RoverListenPorts getEnum(String input) {
+		RoverListenPorts output;
+
+		switch (input) {
+		case "ROVER_10":
+			output = RoverListenPorts.ROVER_10;
+			break;
+		case "ROVER_11":
+			output = RoverListenPorts.ROVER_11;
+			break;
+		case "ROVER_12":
+			output = RoverListenPorts.ROVER_12;
+			break;
+		case "ROVER_13":
+			output = RoverListenPorts.ROVER_13;
+			break;
+		case "ROVER_14":
+			output = RoverListenPorts.ROVER_14;
+			break;
+		case "ROVER_15":
+			output = RoverListenPorts.ROVER_15;
+			break;
+		case "ROVER_16":
+			output = RoverListenPorts.ROVER_16;
+			break;
+		case "ROVER_17":
+			output = RoverListenPorts.ROVER_17;
+			break;
+		case "ROVER_18":
+			output = RoverListenPorts.ROVER_18;
+			break;
+		default:
+			output = RoverListenPorts.DEFAULT;
+		}
+		return output;
+	}
 }
--- src/testRoverComm/R2rApp.java
+++ src/testRoverComm/R2rApp.java
@@ -5,29 +5,49 @@ import enums.RoverListenPorts;
 public class R2rApp {
 	public static void main(String[] args) throws Exception {
 		ROVER_TEST rover10 = new ROVER_TEST("ROVER_10");
-		ROVER_TEST rover00 = new ROVER_TEST("ROVER_00");
-		ROVER_TEST rover99 = new ROVER_TEST("ROVER_99");
+		ROVER_TEST rover11 = new ROVER_TEST("ROVER_11");
+		ROVER_TEST rover12 = new ROVER_TEST("ROVER_12");
+		ROVER_TEST rover13 = new ROVER_TEST("ROVER_13");
+		ROVER_TEST rover14 = new ROVER_TEST("ROVER_14");
+		ROVER_TEST rover15 = new ROVER_TEST("ROVER_15");
+		ROVER_TEST rover16 = new ROVER_TEST("ROVER_16");
+		ROVER_TEST rover17 = new ROVER_TEST("ROVER_17");
+		ROVER_TEST rover18 = new ROVER_TEST("ROVER_18");
+		
+		//start the rover servers
 		rover10.startRoverServer();
-		rover00.startRoverServer();
-		rover99.startRoverServer();
+		rover11.startRoverServer();
+		rover12.startRoverServer();
+		rover13.startRoverServer();
+		rover14.startRoverServer();
+		rover15.startRoverServer();
+		rover16.startRoverServer();
+		rover17.startRoverServer();
+		rover18.startRoverServer();
 		
 		String ipAddr = rover10.getIP();
 		System.out.println("my ip address..." + ipAddr);
 		//test if a rover can connect to other rovers and send msg to them
-		rover10.connect(ipAddr, RoverListenPorts.ROVER_00.getPort());
-		
+		rover10.connect(ipAddr, RoverListenPorts.ROVER_11.getPort());
+		rover10.connect(ipAddr, RoverListenPorts.ROVER_12.getPort());
+		rover10.connect(ipAddr, RoverListenPorts.ROVER_13.getPort());
+		rover10.connect(ipAddr, RoverListenPorts.ROVER_14.getPort());
+		rover10.connect(ipAddr, RoverListenPorts.ROVER_15.getPort());
+		rover10.connect(ipAddr, RoverListenPorts.ROVER_16.getPort());
+		rover10.connect(ipAddr, RoverListenPorts.ROVER_17.getPort());
+		rover10.connect(ipAddr, RoverListenPorts.ROVER_18.getPort());
+				
 		//allow some time to connect before showing what rovers rover10 is connected to
-		Thread.sleep(2000);
+		Thread.sleep(1000);
+		
+		System.out.println("Rover 10's peers: ");
 		rover10.displayList();
-		//by the time rover10 is connceted, rover00 will too, so the list should display
-		rover00.displayList();
-	
-		rover10.connect(ipAddr, RoverListenPorts.ROVER_99.getPort());
+
 		Thread.sleep(2000);
 //		System.out.println("rover 99's connected peers...");
-		rover99.displayList();
+
 		rover10.sendMessage(1, "ROVER hello rover 00");
-		rover00.sendMessage(1, "ROVER oh hello rover 10 from rover 00!");
-		rover99.sendMessage(1, "ROVER hello rover 10 from rover 99!");
+		rover11.sendMessage(1, "ROVER oh hello rover 10 from rover 11!");
+		rover12.sendMessage(1, "ROVER hello rover 10 from rover 12!");
 	}
 }
